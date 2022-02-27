fceb40b182a8b9093438bd5536a4b0e5bb287ba8
lipicoder <lipeng@zepan.com.cn>
2022-02-27 20:34:25 +0800

    layout

--- src/block.js
+++ src/block.js
@@ -203,30 +203,13 @@ var editor = new EditorJS({
         },
       },
       {
-        type: "paragraph",
-        data: {
-          text: 'Workspace in classic editors is made of a single contenteditable element, used to create different HTML markups. Editor.js <mark class="cdx-marker">workspace consists of separate Blocks: paragraphs, headings, images, lists, quotes, etc</mark>. Each of them is an independent contenteditable element (or more complex structure) provided by Plugin and united by Editor\'s Core.',
-        },
-      },
-      {
-        type: "paragraph",
-        data: {
-          text: `There are dozens of <a href="https://github.com/editor-js">ready-to-use Blocks</a> and the <a href="https://editorjs.io/creating-a-block-tool">simple API</a> for creation any Block you need. For example, you can implement Blocks for Tweets, Instagram posts, surveys and polls, CTA-buttons and even games.`,
-        },
-      },
-      {
         type: "header",
         data: {
           text: "What does it mean clean data output",
           level: 3,
         },
       },
-      {
-        type: "paragraph",
-        data: {
-          text: "Classic WYSIWYG-editors produce raw HTML-markup with both content data and content appearance. On the contrary, Editor.js outputs JSON object with data of each Block. You can see an example below",
-        },
-      },
+
       {
         type: "paragraph",
         data: {
@@ -234,20 +217,8 @@ var editor = new EditorJS({
         },
       },
       {
-        type: "paragraph",
-        data: {
-          text: "Clean data is useful to sanitize, validate and process on the backend.",
-        },
-      },
-      {
         type: "delimiter",
         data: {},
-      },
-      {
-        type: "paragraph",
-        data: {
-          text: "We have been working on this project more than three years. Several large media projects help us to test and debug the Editor, to make its core more stable. At the same time we significantly improved the API. Now, it can be used to create any plugin for any task. Hope you enjoy. 😏",
-        },
       },
       {
         type: "image",
