4e84331d40733a4b207f4dba990060346f3d775f
lipicoder <lipeng@zepan.com.cn>
2022-02-27 22:10:56 +0800

    test image

--- src/block.js
+++ src/block.js
@@ -12,7 +12,8 @@ const InlineCode = require('@editorjs/inline-code');
 const LinkTool = require('@editorjs/link');
 import Embed from '@editorjs/embed';
 import Table from '@editorjs/table';
-
+const image_token = "Bearer YUz82BUGJTjFyXOGY22HxtX9atNwJdo2IasOs9XF"
+const file_endpoint = "https://api.cloudflare.com/client/v4/accounts/493524dadc6a7fdef16df9d2ad442576/images/v1"
 
 /**
  * To initialize the Editor, create a new instance with configuration object
@@ -63,42 +64,51 @@ var editor = new EditorJS({
         /**
          * Custom uploader
          */
+        endpoints: {
+          byFile: file_endpoint,
+          byUrl: '',
+        },
+        additinalRequestHeaders: {
+          "Authorization": image_token,
+          "Content-Type": "application/json"
+        },
         uploader: {
           /**
            * Upload file to the server and return an uploaded image data
            * @param {File} file - file selected from the device or pasted by drag-n-drop
            * @return {Promise.<{success, file: {url}}>}
            */
-          uploadByFile(file) {
-            // your own uploading logic here
-            return MyAjax.upload(file).then(() => {
-              return {
-                success: 1,
-                file: {
-                  url: "https://codex.so/upload/redactor_images/o_80beea670e49f04931ce9e3b2122ac70.jpg",
-                  // any other image data you want to store, such as width, height, color, extension, etc
-                },
-              };
-            });
-          },
+          // uploadByFile(file) {
+          //   // your own uploading logic here
+
+          //   return MyAjax.upload(file).then(() => {
+          //     return {
+          //       success: 1,
+          //       file: {
+          //         url: "https://codex.so/upload/redactor_images/o_80beea670e49f04931ce9e3b2122ac70.jpg",
+          //         // any other image data you want to store, such as width, height, color, extension, etc
+          //       },
+          //     };
+          //   });
+          // },
 
           /**
            * Send URL-string to the server. Backend should load image by this URL and return an uploaded image data
            * @param {string} url - pasted image URL
            * @return {Promise.<{success, file: {url}}>}
            */
-          uploadByUrl(url) {
-            // your ajax request for uploading
-            return MyAjax.upload(file).then(() => {
-              return {
-                success: 1,
-                file: {
-                  url: "https://codex.so/upload/redactor_images/o_e48549d1855c7fc1807308dd14990126.jpg",
-                  // any other image data you want to store, such as width, height, color, extension, etc
-                },
-              };
-            });
-          },
+          // uploadByUrl(url) {
+          //   // your ajax request for uploading
+          //   return MyAjax.upload(file).then(() => {
+          //     return {
+          //       success: 1,
+          //       file: {
+          //         url: "https://codex.so/upload/redactor_images/o_e48549d1855c7fc1807308dd14990126.jpg",
+          //         // any other image data you want to store, such as width, height, color, extension, etc
+          //       },
+          //     };
+          //   });
+          // },
         },
       },
     },
@@ -164,72 +174,7 @@ var editor = new EditorJS({
    */
   data: {
     blocks: [
-      {
-        type: "header",
-        data: {
-          text: "Editor.js",
-          level: 2,
-        },
-      },
-      {
-        type: "paragraph",
-        data: {
-          text: "Hey. Meet the new Editor. On this page you can see it in action — try to edit this text. Source code of the page contains the example of connection and configuration.",
-        },
-      },
-      {
-        type: "header",
-        data: {
-          text: "Key features",
-          level: 3,
-        },
-      },
-      {
-        type: "list",
-        data: {
-          items: [
-            "It is a block-styled editor",
-            "It returns clean data output in JSON",
-            "Designed to be extendable and pluggable with a simple API",
-          ],
-          style: "unordered",
-        },
-      },
-      {
-        type: "header",
-        data: {
-          text: "What does it mean «block-styled editor»",
-          level: 3,
-        },
-      },
-      {
-        type: "header",
-        data: {
-          text: "What does it mean clean data output",
-          level: 3,
-        },
-      },
 
-      {
-        type: "paragraph",
-        data: {
-          text: `Given data can be used as you want: render with HTML for <code class="inline-code">Web clients</code>, render natively for <code class="inline-code">mobile apps</code>, create markup for <code class="inline-code">Facebook Instant Articles</code> or <code class="inline-code">Google AMP</code>, generate an <code class="inline-code">audio version</code> and so on.`,
-        },
-      },
-      {
-        type: "delimiter",
-        data: {},
-      },
-      {
-        type: "image",
-        data: {
-          url: "assets/codex2x.png",
-          caption: "",
-          stretched: false,
-          withBorder: true,
-          withBackground: false,
-        },
-      },
     ],
   },
   onReady: function () {
